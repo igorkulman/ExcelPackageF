f1718a17e5998fe99039b0d2988288bab5b903a9
Igor Kulman <igor@kulman.sk>
2013-02-06 16:01:36 +0100

    init commit

---
+++ ExcelPackageF/ExcelPackageF.fs
@@ -0,0 +1,51 @@
+﻿namespace ExcelPackageF
+
+open OfficeOpenXml
+open System
+open System.IO
+open System.Xml.XPath
+open System.Xml
+
+module Excel =
+
+    let getWorksheet (index:int) filename = 
+        let file = new FileInfo(filename) 
+        let xlPackage = new ExcelPackage(file)
+        xlPackage.Workbook.Worksheets.[index] 
+
+    let getMaxRowNumber (worksheet:ExcelWorksheet) = 
+        let nav = worksheet.WorksheetXml.CreateNavigator()
+        let exp = nav.Compile("//*[name()='row']/@r")
+        exp.AddSort("../@r", XmlSortOrder.Descending, XmlCaseOrder.None, "", XmlDataType.Number)
+        let node = nav.SelectSingleNode(exp).UnderlyingObject :?> XmlNode
+        int node.InnerText;  
+
+    let getMaxColNumber (worksheet:ExcelWorksheet) = 
+        let nav = worksheet.WorksheetXml.CreateNavigator()
+        let exp = nav.Compile("//*[name()='c']/@colNumber")
+        exp.AddSort("../@colNumber", XmlSortOrder.Descending, XmlCaseOrder.None, "", XmlDataType.Number)
+        let node = nav.SelectSingleNode(exp).UnderlyingObject :?> XmlNode
+        int node.InnerText;  
+
+    let getContent worksheet = seq {        
+        let maxRow = getMaxRowNumber worksheet
+        let maxCol = getMaxColNumber worksheet
+        for i in 1..maxRow do
+            for j in 1..maxCol do
+                let content = worksheet.Cell(i,j).Value
+                yield content
+    }
+
+    let getColumn colIndex (worksheet:ExcelWorksheet) = seq { 
+        let maxRow = getMaxRowNumber worksheet  
+        for i in 1..maxRow do        
+            let content = worksheet.Cell(i,colIndex).Value
+            yield content
+    }
+
+    let getRow rowIndex (worksheet:ExcelWorksheet) = seq { 
+        let maxCol = getMaxColNumber worksheet  
+        for i in 1..rowIndex do        
+            let content = worksheet.Cell(rowIndex,maxCol).Value
+            yield content
+    }
---
+++ ExcelPackageF/lib/GacReg.bat
@@ -0,0 +1,1 @@
+"%programfiles%\Microsoft Visual Studio 8\SDK\v2.0\Bin\gacutil.exe" -i ExcelPackage.dll
